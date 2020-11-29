7b4b8e7661226e21bb507e9c50b2b9bf257a52f9
nafifurqon <muhammadnafi98@gmail.com>
2020-11-29 20:29:08 +0700

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://nafi-devcamp.herokuapp.com', 'http://nafi-devcamp.herokuapp.com']
+  config.action_cable.url = "wss://nafi-devcamp.herokuapp.com/cable"
 end
